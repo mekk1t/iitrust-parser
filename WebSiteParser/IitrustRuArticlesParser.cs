844a3bd4dadb721d5c201cc5b67cd6370a1a1979
mekk1t <kit.kalimov@gmail.com>
2021-01-23 18:09:54 +0300

    Внедрение зависимостей в IitrustRuArticlesParser

--- WebSiteParser/IitrustRuArticlesParser.cs
+++ WebSiteParser/IitrustRuArticlesParser.cs
@@ -10,14 +10,20 @@ namespace WSP
         private readonly IXmlConverter<XmlConvertRequest, XmlConvertResponse> converter;
         private readonly IResultSaver resultSaver;
 
+        public IitrustRuArticlesParser(
+            IPageParser<IEnumerable<ArticlesParseResult>> parser,
+            IXmlConverter<XmlConvertRequest, XmlConvertResponse> converter,
+            IResultSaver resultSaver)
+        {
+            this.parser = parser;
+            this.converter = converter;
+            this.resultSaver = resultSaver;
+        }
+
         public void ParseArticlesFromSite(string siteUrl)
         {
             var result = parser.GetPageContent(siteUrl);
-            var xmlResult = converter.ConvertToXml(new XmlConvertRequest
-            {
-                Data = result
-            });
-
+            var xmlResult = converter.ConvertToXml(new XmlConvertRequest(result));
             resultSaver.SaveResult(xmlResult.ToString());
         }
     }
