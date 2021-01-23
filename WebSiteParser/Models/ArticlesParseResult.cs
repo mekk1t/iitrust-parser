5f57c70d68c658cb3d55efe827a0abcd30980719
mekk1t <kit.kalimov@gmail.com>
2021-01-23 18:06:10 +0300

    Предусмотрение ситуации, когда у статей нет картинки

--- WebSiteParser/Models/ArticlesParseResult.cs
+++ WebSiteParser/Models/ArticlesParseResult.cs
@@ -2,17 +2,40 @@
 {
     public class ArticlesParseResult
     {
+        private string placeholderImage;
+
+        private readonly string imageBase64;
+
         public string Title { get; }
         public string Url { get; }
-        public string ImageBase64 { get; }
+        public string ImageBase64
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(imageBase64))
+                {
+                    if (!string.IsNullOrWhiteSpace(placeholderImage))
+                        return placeholderImage;
+
+                    return string.Empty;
+                }
+
+                return imageBase64;
+            }
+        }
         public string Content { get; }
 
         public ArticlesParseResult(string title, string url, string imageBase64, string content)
         {
             this.Title = title;
             this.Url = url;
-            this.ImageBase64 = imageBase64;
+            this.imageBase64 = imageBase64;
             this.Content = content;
+        }
+
+        public void SetPlaceholderImage(string placeholderImage)
+        {
+            this.placeholderImage = placeholderImage;
         }
     }
 }
