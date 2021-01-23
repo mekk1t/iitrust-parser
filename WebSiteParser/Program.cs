f56384194c23d9e59fe2de40d3a817197f723807
mekk1t <kit.kalimov@gmail.com>
2021-01-23 18:10:08 +0300

    Заглушка для парсера, чтобы проект компилировался

--- WebSiteParser/Program.cs
+++ WebSiteParser/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace WSP
+﻿using System.Collections.Generic;
+using WSP.Abstractions;
+using WSP.Models;
+
+namespace WSP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var iitrustRuArticlesParser = new IitrustRuArticlesParser();
+            var iitrustRuArticlesParser = new IitrustRuArticlesParser(
+                new object() as IPageParser<IEnumerable<ArticlesParseResult>>,
+                new object() as IXmlConverter<XmlConvertRequest, XmlConvertResponse>,
+                new object() as IResultSaver);
             iitrustRuArticlesParser.ParseArticlesFromSite("http://iitrust.ru/articles");
         }
     }
