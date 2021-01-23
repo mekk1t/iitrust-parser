c943cf1fce948f47026951211fcd17ed252e156b
mekk1t <61093487+mekk1t@users.noreply.github.com>
2021-01-23 15:28:53 +0300

    УРОК 01. Что такое AngleSharp и как парсить страницы на C#. (#1)
    * Первый урок
    
    * Проект переехал на уровень выше, в папку с другим названием
    
    * Перезапись файла решения
    
    * Смена исполняемой среды и задан неймспейс по умолчанию
    
    * Смена названия расширения + рефакторинг

---
+++ WebSiteParser/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+
+namespace IitrustParser.Extensions
+{
+    public static class StringExtensions
+    {
+        public static IDocument AsIDocument(this string rawHtml)
+        {
+            return BrowsingContext
+                .New()
+                .OpenAsync(r => r.Content(rawHtml))
+                .Result;
+        }
+    }
+}
--- WebSiteParser/Program.cs
+++ WebSiteParser/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using AngleSharp.Dom;
+using IitrustParser.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 
-namespace WSP
+namespace IitrustParser
 {
     class Program
     {
+        private const string BASE_URL = "https://iitrust.ru";
+        public static List<ArticlePreview> ArticlePreviews { get; set; } = new List<ArticlePreview>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.GetAsync($"{BASE_URL}/articles").Result;
+
+                IDocument document = response.Content.ReadAsStringAsync().Result.AsIDocument();
+
+                var elements = document.All;
+
+                var articlePreviewTitles = elements.Where(e => e.ClassList.Contains("iit-article"));
+
+                foreach (var preview in articlePreviewTitles)
+                {
+                    var link = preview.QuerySelector("a");
+                    var title = preview.QuerySelector(".iit-article__title");
+                    var theme = preview.QuerySelector(".iit-article__background");
+
+                    var imageUrl = preview.QuerySelector(".iit-article__img");
+
+                    var articlePreview = new ArticlePreview
+                    {
+                        Title = title.Text(),
+                        DetailsUrl = link.GetAttribute("href"),
+                        ImageUrl = imageUrl.GetAttribute("data-src"),
+                        Theme = theme.ClassList.Where(c => c != "iit-article__background").First()
+                    };
+
+                    ArticlePreviews.Add(articlePreview);
+                }
+            }
         }
     }
 }
