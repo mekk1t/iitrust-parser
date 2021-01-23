21bc7707011a2f13b419e2bfeafc245041ee6470
mekk1t <kit.kalimov@gmail.com>
2021-01-23 15:59:04 +0300

    Реализация + интерфейс парсера

---
+++ WebSiteParser/Interfaces/IParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WSP.Interfaces
+{
+    public interface IParser<TResult>
+    {
+        TResult ParseSite(string siteUrl);
+        void SaveParseResult(TResult result);
+        void SaveParseResult(IEnumerable<TResult> results);
+    }
+}
---
+++ WebSiteParser/Parser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using WSP.Interfaces;
+
+namespace WSP
+{
+    public class Parser : IParser<string>
+    {
+        public string ParseSite(string siteUrl)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveParseResult(string result)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveParseResult(IEnumerable<string> results)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
