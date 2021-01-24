c6e28c991e85b4f01b7963ca7c8a232080bfdf54
mekk1t <kit.kalimov@gmail.com>
2021-01-24 11:07:05 +0300

    Update Program.cs

--- WebSiteParser/Program.cs
+++ WebSiteParser/Program.cs
@@ -9,11 +9,6 @@ namespace WSP
     {
         static void Main(string[] args)
         {
-            //var iitrustRuArticlesParser = new IitrustRuArticlesParser(
-            //    new object() as IPageParser<IEnumerable<ArticlesParseResult>>,
-            //    new object() as IXmlConverter<XmlConvertRequest, XmlConvertResponse>,
-            //    new object() as IResultSaver);
-            //iitrustRuArticlesParser.ParseArticlesFromSite("http://iitrust.ru/articles");
         }
     }
 }
