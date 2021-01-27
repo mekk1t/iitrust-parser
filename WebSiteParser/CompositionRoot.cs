c692e6dec712ffc8a1831975fd5fec9544153cfb
mekk1t <kit.kalimov@gmail.com>
2021-01-27 08:45:34 +0300

    Update CompositionRoot.cs

--- WebSiteParser/CompositionRoot.cs
+++ WebSiteParser/CompositionRoot.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WSP
+﻿namespace WSP
 {
     public class CompositionRoot
     {
