89542dbfe8d6a5c11c5c51ba9fbd953a0c3acd4b
mekk1t <kit.kalimov@gmail.com>
2021-01-27 08:47:07 +0300

    Модели для парсинга

---
+++ ArticlesParser/Models/ArticlePage.cs
@@ -0,0 +1,11 @@
+﻿using Kernel.Abstractions;
+
+namespace ArticlesParser.Models
+{
+    public class ArticlePage : IArticle
+    {
+        public string Content { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ ArticlesParser/Models/ArticleSection.cs
@@ -0,0 +1,13 @@
+﻿using Kernel.Abstractions;
+using System.Collections.Generic;
+
+namespace ArticlesParser.Models
+{
+    public class ArticleSection
+    {
+        public string Title { get; }
+        public string OldImageBase64 { get; set; }
+        public string NewImageBase64 { get; set; }
+        public IEnumerable<IArticle> Articles { get; set; }
+    }
+}
