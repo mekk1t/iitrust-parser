2a2aeffa13157bb032159dd59725ef8e18f726ec
mekk1t <kit.kalimov@gmail.com>
2021-01-27 08:51:33 +0300

    Проект тестов для проверки работоспособности классов. Ничего серьезного, лишь в качестве вспомогательного инструмента

---
+++ ArticleParserTests/ArticleSectionParserTests.cs
@@ -0,0 +1,23 @@
+﻿using ArticlesParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArticleParserTests
+{
+    public class ArticleSectionParserTests
+    {
+        [Fact]
+        public void Foo()
+        {
+            var parser = new ArticleSectionParser(new ArticlePageParser());
+
+            var result = parser.GetPageContent("https://iitrust.ru/articles");
+
+
+        }
+    }
+}
