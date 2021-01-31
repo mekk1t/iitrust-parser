ce2dc55dffc1af6586c532770aaeb04b378b51eb
Peter Tsivinsky <creatsiv@gmail.com>
2021-01-31 14:10:56 +0300

    feat: add test example of JsonToXmlConverter

---
+++ JsonToXmlConverter/Converter.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml.Linq;
+
+namespace JsonToXmlConverter
+{
+    public class Converter
+    {
+        static string convert(string jsonString)
+        {
+            XNode node = JsonConvert.DeserializeXNode(jsonString, "Root");
+
+            return node.ToString();
+        }
+    }
+}
