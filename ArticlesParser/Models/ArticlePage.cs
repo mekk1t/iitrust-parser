0e0fd92cceb8896fe966647d0507dbe4a7769f24
mekk1t <kit.kalimov@gmail.com>
2021-02-01 21:01:13 +0300

    Добавил поле изображения в модель статьи

--- ArticlesParser/Models/ArticlePage.cs
+++ ArticlesParser/Models/ArticlePage.cs
@@ -7,5 +7,6 @@ namespace ArticlesParser.Models
         public string Content { get; set; }
         public string Url { get; set; }
         public string Title { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
