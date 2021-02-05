814d65c6bc027a195931bf90e929ad6dedeb0d09
devskyfly <devskyfly@gmail.com>
2021-02-05 20:26:27 +0300

    Add data folder.

--- Implementations/ResultSaver/ResultSaver.cs
+++ Implementations/ResultSaver/ResultSaver.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using WSP.Abstractions;
 
-namespace Implementations.IResultSaver
+namespace Implementations
 {
-    public class ResultSaver
+    public class ResultSaver: IResultSaver<string>
     {
-        
+        public void SaveResult(string result)
+        {
+            var dir = Directory.GetCurrentDirectory();
+            var path = Directory.GetCurrentDirectory();
+            var targetDir = $"{path}/data";
+            var targetFile = $"{path}/data/file";
+            File.WriteAllText(targetFile, "Data");
+        }
     }
 }
