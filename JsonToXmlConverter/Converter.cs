3e56eae5f4b4e7c6149db406eeadc9c8c70052c2
wowxoxo <31386682+wowxoxo@users.noreply.github.com>
2021-02-06 18:25:10 +0300

    feat: add test example of JsonToXmlConverter (#4)
    * feat: add test example of JsonToXmlConverter
    
    * Fix converter
    
    * fix: using
    
    Co-authored-by: mekk1t <61093487+mekk1t@users.noreply.github.com>

---
+++ JsonToXmlConverter/Converter.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Linq;
+using WSP.Abstractions;
+using WSP.Models;
+using System.Collections.Generic;
+using Kernel.Abstractions;
+
+namespace JsonToXmlConverter
+{
+    public class Converter: IXmlConverter<XmlConvertRequest, XmlConvertResponse>
+    {
+        public XmlConvertResponse ConvertToXml(XmlConvertRequest req)
+        {
+            IEnumerable<IArticle> data = req.Data;
+            string jsonString = JsonConvert.SerializeObject(data);
+
+            string result = ConvertJsonToXml(jsonString);
+
+            return new XmlConvertResponse(result);
+        }
+
+        private string ConvertJsonToXml(string jsonString)
+        {
+             XNode node = JsonConvert.DeserializeXNode(jsonString, "Root");
+
+            return node.ToString();
+        }
+    }
+}
